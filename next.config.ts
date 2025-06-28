5b592f0553461d7978c806d40294d89fba4da38b
Aydin Asilnezhad <aydinaslinezhad@gmail.com>
2025-06-28 22:31:47 +0000

    اگر ایده 2 هم اجرا کردی برو سراغ ایده5 و بعد برو سراغ یک دیباگ اساسی در

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
 
 import type {NextConfig} from 'next';
 
+const contentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' https://placehold.co data:;
+  font-src 'self';
+  connect-src 'self';
+  frame-ancestors 'none';
+  form-action 'self';
+  base-uri 'self';
+  object-src 'none';
+`.replace(/\s{2,}/g, ' ').trim();
+
+
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -34,6 +48,19 @@ const nextConfig: NextConfig = {
       config.resolve.fallback.async_hooks = false;
     }
     return config;
+  },
+   async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: contentSecurityPolicy,
+          },
+        ],
+      },
+    ]
   },
 };
 
